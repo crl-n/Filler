2771c0841533a5bd728078ec03611ed2065ada92
Carl Nysten <nysten.carl@gmail.com>
2022-04-10 12:10:33 +0300

    frame based visualisation

--- visualiser.py
+++ visualiser.py
@@ -10,7 +10,9 @@ NEXTLINE = ESC + '[1E'
 SET_COLOR = ESC + '[38;5;{0}m'
 CLEAR = ESC + '[2J' + GOTO_ORIGIN
 
-out = sys.stdout
+RED = 0xe0
+GREEN = 0x1c
+BLUE = 0x3
 
 banner_thin = ['███ █ █  █  ███ ███',
                '█   █ █  █  █   █ █',
@@ -24,14 +26,102 @@ banner = ['████ ██ ██  ██  ████ ████',
           '██   ██ ██  ██  ██   ██ █',
           '██   ██ ███ ███ ████ ██ █']
 
-out.write(HIDE_CURSOR)
-while (1):
-    for i in range(168, 228):
-        out.write(SET_COLOR.format(i))
+def itorgb(i):
+    r = (i & 0xe0) >> 5
+    g = (i & 0x1c) >> 2
+    b = i & 0x3
+    return (r, g, b)
+
+def rgbtoi(r, g, b):
+    i = r << 5
+    i += g << 2
+    i += b
+    return i
+
+# ISSUE: Too big l-values cause flicker
+# ISSUE: b has to be bigger than a
+def gradient(a, b, l):
+    r1, g1, b1 = itorgb(a)
+    r2, g2, b2 = itorgb(b)
+    sr = r2 - r1 // l
+    sg = g2 - g1 // l
+    sb = b2 - b1 // l
+    colors = []
+    for i in range(l):
+        r = r1 + i * sr
+        g = g1 + i * sg
+        b = b1 + i * sb
+        colors.append(rgbtoi(r, g, b))
+    return colors
+
+def get_player_numbers():
+    for _ in range(5):
+        sys.stdin.readline()
+    p1 = 'Player 1: '
+    p2 = 'Player 2: '
+    p1_split = sys.stdin.readline().rsplit('/')
+    p1 += p1_split[-1].rstrip('\n')
+    sys.stdin.readline()
+    p2_split = sys.stdin.readline().rsplit('/')
+    p2 += p2_split[-1].rstrip('\n')
+    sys.stdin.readline()
+    print(p1, p2)
+    return (p1, p2)
+
+def get_dims():
+    parts = sys.stdin.readline().rstrip(':\n').split(' ')
+    rows = int(parts[1])
+    cols = int(parts[2])
+    return (rows, cols)
+
+def get_frames(rows):
+    i = 0
+    frames = []
+    frame = []
+    for line in sys.stdin:
+        if 'Plateau' in line:
+            i = 0
+            continue
+        if i == 0:
+            i += 1
+            continue
+        if i <= rows:
+            line = line.split(' ')[1]
+            frame.append(line)
+        if i == rows:
+            frames.append(frame)
+            frame = []
+        i += 1
+    return frames
+
+def main():
+    out = sys.stdout
+    p1, p2 = get_player_numbers()
+    rows, cols = get_dims()
+    frames = get_frames(rows)
+    out.write(HIDE_CURSOR)
+    for color in gradient(0, 255, 20):
+        out.write(SET_COLOR.format(color))
         for row in banner:
             out.write(row)
             out.write(NEXTLINE)
         out.flush()
-        sleep(0.001)
+        sleep(0.01)
         out.write(CLEAR)
-out.write(SHOW_CURSOR)
+    for frame in frames:
+        for row in banner:
+            out.write(row)
+            out.write(NEXTLINE)
+        out.write(SET_COLOR.format(231))
+        out.write(NEXTLINE)
+        out.write(NEXTLINE)
+        for line in frame:
+            out.write(line)
+            out.write(NEXTLINE)
+        out.flush()
+        sleep(0.1)
+        out.write(CLEAR)
+    out.write(SHOW_CURSOR)
+
+if __name__ == '__main__':
+    main()
