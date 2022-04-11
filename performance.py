c0bcfaa384e5384e8d713a343baa82ea681d320e
Carl Nysten <nysten.carl@gmail.com>
2022-04-11 11:23:56 +0300

    performance tests uses map_async

--- performance.py
+++ performance.py
@@ -3,16 +3,20 @@ import os
 import time
 from multiprocessing.pool import ThreadPool
 import multiprocessing as mp
-from subprocess import Popen
 import pandas as pd
-from tqdm import tqdm
 
+# TODO: Add error handling for when players fail to finish a game.
+             #'grati.filler',
 opponents = ['champely.filler',
              'hcao.filler',
-             'grati.filler']
-maps = ['map00', 'map01']
-n_of_tests = len(maps) * len(opponents) * 5
-#progress_bar = tqdm(total=n_of_tests)
+             'carli.filler',
+             'abanlin.filler',
+             'superjeannot.filler']
+maps = ['map00', 'map01', 'map02']
+GAMES_PER_OPPONENT = 5
+N_OF_TESTS = len(maps) * len(opponents) * GAMES_PER_OPPONENT
+BOLD = '\033[1m'
+RESET = '\033[0m'
 
 def get_result(p):
     for i in range(7):
@@ -21,12 +25,10 @@ def get_result(p):
     x_result = int(p.readline().split(':')[1])
     return (o_result, x_result)
 
-def call_proc(filler_vm, player_binary, opponent_binary, opponent, mapfile, m):
+def call_proc(args):
+    filler_vm, player_binary, opponent_binary, opponent, mapfile, m = args
     p = os.popen('{} -q -p1 {} -p2 {} -f {}'.format(filler_vm, player_binary, opponent_binary, mapfile))
     return (p, (opponent, m))
-
-def update_prog_bar(_):
-    progress_bar.update()
 
 def main():
 
@@ -45,36 +47,34 @@ def main():
     df = pd.DataFrame(columns = ['Player', 'Opponent', 'Player Score', 'Opponent Score', 'Map'])
 
     start = time.time()
-    print('Running matches... Please wait.')
+    print(BOLD + 'Running matches... Please wait.' + RESET)
     pool = ThreadPool(mp.cpu_count())
-    results = []
+    tests = []
     for opponent in opponents:
         opponent_binary = '{}/players/{}'.format(resources_path, opponent)
         for m in maps:
             mapfile = '{}/maps/{}'.format(resources_path, m)
-            processes = []
-            for i in range(5):
-                results.append(pool.apply_async(call_proc, (filler_vm, player_binary, opponent_binary, opponent, mapfile, m)))
+            for i in range(GAMES_PER_OPPONENT):
+                tests.append((filler_vm, player_binary, opponent_binary, opponent, mapfile, m))
+                results = pool.map_async(call_proc, tests)
 
     pool.close()
     pool.join()
 
-    for r in results:
-        p, args = r.get()
+    for r in results.get():
+        p, args = r
         o, x = get_result(p)
         opponent, m = args
         d = {'Player' : player, 'Opponent' : opponent, 'Player Score' : o, 'Opponent Score' : x, 'Map':m}
         df = pd.concat([df, pd.DataFrame([d])], ignore_index=True, axis = 0)
 
     end = time.time()
-    time.sleep(0.5)
+
     print(df)
     print('Execution time: {}'.format(end - start))
     
-    # TODO: Make a function that extracts the match result and other relevant metrics from temp output
     # TODO: Figure out if virtual machine can be modified to speed up games
     # TODO: Plot/report results
-
     # TODO: Create folder for log files and implement logging of dataframe.
     #log = 'performance_log_{}'.format(time.strftime('%d%m%Y-%H%M', time.localtime()))
 
